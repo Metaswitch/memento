e6ebbe2ea3db7558f2c12ccd5b5eb07c9d0edda6
Eleanor Merry <eleanor.merry@metaswitch.com>
2014-07-25 09:27:53 +0000

    Add missed include

---
+++ include/mementoappserver.h
@@ -0,0 +1,224 @@
+/**
+ * @file mementoappserver.h
+ *
+ * Project Clearwater - IMS in the cloud.
+ * Copyright (C) 2014  Metaswitch Networks Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version, along with the "Special Exception" for use of
+ * the program along with SSL, set forth below. This program is distributed
+w
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * The author can be reached by email at clearwater@metaswitch.com or by
+ * post at Metaswitch Networks Ltd, 100 Church St, Enfield EN2 6BQ, UK
+ *
+ * Special Exception
+ * Metaswitch Networks Ltd  grants you permission to copy, modify,
+ * propagate, and distribute a work formed by combining OpenSSL with The
+ * Software, or a work derivative of such a combination, even if such
+ * copying, modification, propagation, or distribution would otherwise
+ * violate the terms of the GPL. You must comply with the GPL in all
+ * respects for all of the code used other than OpenSSL.
+ * "OpenSSL" means OpenSSL toolkit software distributed by the OpenSSL
+ * Project and licensed under the OpenSSL Licenses, or a work based on such
+ * software and licensed under the OpenSSL Licenses.
+ * "OpenSSL Licenses" means the OpenSSL License and Original SSLeay License
+ * under which the OpenSSL Project distributes the OpenSSL toolkit software,
+ * as those licenses appear in the file LICENSE-OPENSSL.
+ */
+
+#ifndef MEMENTOAPPSERVER_H__
+#define MEMENTOAPPSERVER_H__
+
+extern "C" {
+#include <pjsip.h>
+#include <pjsip/sip_msg.h>
+#include <pjlib-util.h>
+#include <pjlib.h>
+}
+
+#include "appserver.h"
+#include "load_monitor.h"
+#include "call_list_store_processor.h"
+#include "sas.h"
+
+class MementoAppServer;
+class MementoAppServerTsx;
+
+/// The MementoAppServer implements the Memento service, and subclasses
+/// the abstrat AppServer class.
+///
+/// Sprout calls the get_app_tsx method on MementoAppServer when
+///
+/// -  an IFC triggers with ServiceName containing a host name of the form
+///    memento@<homedomain>;
+/// -  a request is received for a dialog where the service previously called
+///    add_to_dialog.
+///
+class MementoAppServer : public AppServer
+{
+public:
+  /// Constructor.
+  /// @param  service_name          - Service name (memento).
+  /// @param  home_domain           - Home domain of deployment (from configuration).
+  /// @param  max_call_list_length  - Maximum number of calls to store (from configuration).
+  /// @param  memento_thread        - Number of memento threads (from configuration).
+  /// @param  call_list_ttl         - Time to store calls in Cassandra (from configuration).
+  MementoAppServer(const std::string& service_name,
+                   const std::string& home_domain,
+                   const int max_call_list_length,
+                   const int memento_thread,
+                   const int call_list_ttl);
+
+  /// Virtual destructor.
+  ~MementoAppServer();
+
+  /// Called when the system determines the service should be invoked for a
+  /// received request.  The AppServer can either return NULL indicating it
+  /// does not want to process the request, or create a suitable object
+  /// derived from the AppServerTsx class to process the request.
+  ///
+  /// @param  helper        - The service helper to use to perform
+  ///                         the underlying service-related processing.
+  /// @param  req           - The received request message.
+  virtual AppServerTsx* get_app_tsx(AppServerTsxHelper* helper,
+                                    pjsip_msg* req);
+
+  /// Returns the name of this service.
+//  const std::string service_name() { return _service_name; }
+
+
+private:
+
+  /// The name of this service.
+  const std::string _service_name;
+
+  /// Home domain of deployment.
+  std::string _home_domain;
+
+  /// Load monitor.
+  LoadMonitor* _load_monitor;
+
+  /// Call list store processor. TODO update comment
+  CallListStore::Store* _call_list_store;
+
+  /// Call list store processor.
+  CallListStoreProcessor* _call_list_store_processor;
+};
+
+/// The MementoAppServerTsx class subcis an abstract base class used to handle the
+/// application-server-specific processing of a single transaction.  It
+/// encapsulates a ServiceTsx, which it calls through to to perform the
+/// underlying service-related processing.
+///
+class MementoAppServerTsx : public AppServerTsx
+{
+public:
+  /// Virtual destructor.
+  virtual ~MementoAppServerTsx();
+
+  /// Called for an initial request (dialog-initiating or out-of-dialog) with
+  /// the original received request for the transaction.  
+  /// This method stores information about the INVITE, and adds itself as a 
+  /// Record-Route header. 
+  /// @param req           - The received initial request.
+  virtual void on_initial_request(pjsip_msg* req);
+
+  /// Called for an in-dialog request with the original received request for
+  /// the transaction.  On return the request will be forwarded within 
+  /// the dialog.
+  /// @param req           - The received in-dialog request.
+  virtual void on_in_dialog_request(pjsip_msg* req);
+
+  /// Called with all responses received on the transaction.  If a transport
+  /// error or transaction timeout occurs on a downstream leg, this method is
+  /// called with a 408 response.  The return value indicates whether the
+  /// response should be forwarded upstream (after suitable consolidation if
+  /// the request was forked).  If the return value is false and new targets
+  /// have been added with the add_target API, the original request is forked
+  /// to them.
+  ///
+  /// @returns             - true if the response should be forwarded upstream
+  ///                        false if the response should be dropped
+  /// @param  rsp          - The received request.
+  /// @param  fork_id      - The identity of the downstream fork on which
+  ///                        the response was received.
+  virtual void on_response(pjsip_msg* rsp, int fork_id);
+
+  void set_members(LoadMonitor* load_monitor,
+                   CallListStore::Store* call_list_store,
+                   CallListStoreProcessor* call_list_store_processor,
+                   std::string& home_domain);
+  /// Constructor.
+  MementoAppServerTsx(AppServerTsxHelper* helper);
+
+private:
+  /// Creates a formatted string YYYYMMDDHHMMSS from a tm
+  /// @param      - The time to convert
+  std::string create_formatted_timestamp(tm* timestamp, const char* pattern);
+
+  std::string uri_to_string(pjsip_uri_context_e context,
+                            const pjsip_uri* uri);
+
+  std::string pj_str_to_string(const pj_str_t* pjstr);
+
+  /// Transaction context to use for underlying service-related processing.
+  ServiceTsxHelper* _service_tsx_helper;
+
+  /// Flag for whether the call has been answered or rejected
+  bool _answered;
+
+  /// Flag for whether the call is incoming or outgoing
+  bool _incoming;
+
+  /// Start time of the call (as measured from receipt of a 200 OK). Stored as
+  /// ms from the epoch as a string? // TODO is this the best?
+  tm* _start_time;
+
+  std::string _start_time_xml;
+  std::string _start_time_store;
+
+  /// Caller name (can be empty)
+  std::string _caller_name;
+
+  /// Caller URI
+  std::string _caller_uri;
+
+  /// Callee name (can be empty)
+  std::string _callee_name;
+
+  /// Callee URI
+  std::string _callee_uri;
+
+  /// Flag for whether a response has already been received on this 
+  /// transaction
+  bool _stored_entry;
+
+  /// Unique identifier for this transaction - generated from the timestamp
+  /// and a random number.
+  std::string _unique_id;
+
+  /// IMPU of the call list owner - TODO make snappier. 
+  std::string _impu;
+
+  LoadMonitor* _load_monitor;
+
+  /// Call list store processor. TODO update comment
+  CallListStore::Store* _call_list_store;
+
+  /// Call list store processor.
+  CallListStoreProcessor* _call_list_store_processor;
+
+  /// Home domain of deployment.
+  std::string _home_domain;
+};
+
+#endif
