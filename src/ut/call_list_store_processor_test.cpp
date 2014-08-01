e71f3898f444b46228a2edbd6bde8f2db0ea0de4
Eleanor Merry <eleanor.merry@metaswitch.com>
2014-08-01 13:15:58 +0000

    Updates after review

--- src/ut/call_list_store_processor_test.cpp
+++ src/ut/call_list_store_processor_test.cpp
@@ -52,6 +52,11 @@ using ::testing::StrictMock;
 using ::testing::Mock;
 using ::testing::DoAll;
 
+static int CALL_LIST_TTL = 604800;
+static int FAKE_SAS_TRAIL = 0;
+static std::string IMPU = "sip:6510001000@home.domain";
+static std::string TIMESTAMP = "20020530093010";
+
 // Fixture for tests that has no limit to the number of stored call lists
 class CallListStoreProcessorTest : public ::testing::Test
 {
@@ -59,7 +64,9 @@ public:
   CallListStoreProcessorTest()
   {
     _cls = new MockCallListStore();
-    _clsp = new CallListStoreProcessor(&_load_monitor, _cls, 0, 2, 604800);
+
+    // No maximum call length and 1 worker thread
+    _clsp = new CallListStoreProcessor(&_load_monitor, _cls, 0, 1, CALL_LIST_TTL);
   }
 
   virtual ~CallListStoreProcessorTest()
@@ -80,7 +87,9 @@ public:
   CallListStoreProcessorWithLimitTest()
   {
     _cls = new MockCallListStore();
-    _clsp = new CallListStoreProcessor(&_load_monitor, _cls, 4, 1, 604800);
+
+    // Maximum call length of 4 and 2 worker threads
+    _clsp = new CallListStoreProcessor(&_load_monitor, _cls, 4, 2, CALL_LIST_TTL);
   }
 
   virtual ~CallListStoreProcessorWithLimitTest()
@@ -95,7 +104,9 @@ public:
 };
 
 // Create a vector of call list store fragments that the mock
-// get_call_fragments_sync can return
+// get_call_fragments_sync can return. It creates 7 records
+// making up 6 calls; the first two match the begin and end of
+// a call, then last five are all single rejected calls.
 void create_records(std::vector<CallListStore::CallFragment>& records)
 {
   CallListStore::CallFragment record1;
@@ -109,102 +120,108 @@ void create_records(std::vector<CallListStore::CallFragment>& records)
   record1.type = CallListStore::CallFragment::Type::BEGIN;
   record1.timestamp = "20020530093010";
   record1.id = "a";
-  record1.contents = (
+  record1.contents =
     "<to>"
-        "<URI>alice@example.com</URI>"
-        "<name>Alice Adams</name>"
-      "</to>"
-      "<from>"
-        "<URI>bob@example.com</URI>"
-        "<name>Bob Barker</name>"
-      "</from>"
-      "<answered>1</answered>"
-      "<outgoing>1</outgoing>"
-      "<start-time>2002-05-30T09:30:10</start-time>"
-    "<answer-time>2002-05-30T09:30:20</answer-time>");
+      "<URI>alice@example.com</URI>"
+      "<name>Alice Adams</name>"
+    "</to>"
+    "<from>"
+      "<URI>bob@example.com</URI>"
+      "<name>Bob Barker</name>"
+    "</from>"
+    "<answered>1</answered>"
+    "<outgoing>1</outgoing>"
+    "<start-time>2002-05-30T09:30:10</start-time>"
+    "<answer-time>2002-05-30T09:30:20</answer-time>";
+
   record2.type = CallListStore::CallFragment::Type::END;
   record2.timestamp = "20020530093010";
   record2.id = "a";
   record2.contents = "<end-time>2002-05-30T09:35:00</end-time>";
+
   record3.type = CallListStore::CallFragment::Type::REJECTED;
   record3.timestamp = "20020530093011";
   record3.id = "b";
-  record3.contents = (
+  record3.contents =
     "<to>"
-        "<URI>alice@example.net</URI>"
-        "<name>Alice Adams</name>"
-      "</to>"
-      "<from>"
-        "<URI>bob@example.net</URI>"
-        "<name>Bob Barker</name>"
-      "</from>"
-      "<answered>0</answered>"
-      "<outgoing>1</outgoing>"
-    "<start-time>2002-05-30T09:30:11</start-time>");
+      "<URI>alice@example.net</URI>"
+      "<name>Alice Adams</name>"
+    "</to>"
+    "<from>"
+      "<URI>bob@example.net</URI>"
+      "<name>Bob Barker</name>"
+    "</from>"
+    "<answered>0</answered>"
+    "<outgoing>1</outgoing>"
+    "<start-time>2002-05-30T09:30:11</start-time>";
+
   record4.type = CallListStore::CallFragment::Type::REJECTED;
   record4.timestamp = "20020530093012";
   record4.id = "c";
-  record4.contents = (
+  record4.contents =
     "<to>"
-        "<URI>alice@example.net</URI>"
-        "<name>Alice Adams</name>"
-      "</to>"
-      "<from>"
-        "<URI>bob@example.net</URI>"
-        "<name>Bob Barker</name>"
-      "</from>"
-      "<answered>0</answered>"
-      "<outgoing>1</outgoing>"
-    "<start-time>2002-05-30T09:30:12</start-time>");
+      "<URI>alice@example.net</URI>"
+      "<name>Alice Adams</name>"
+    "</to>"
+    "<from>"
+      "<URI>bob@example.net</URI>"
+      "<name>Bob Barker</name>"
+    "</from>"
+    "<answered>0</answered>"
+    "<outgoing>1</outgoing>"
+    "<start-time>2002-05-30T09:30:12</start-time>";
+
   record5.type = CallListStore::CallFragment::Type::REJECTED;
   record5.timestamp = "20020530093013";
   record5.id = "d";
-  record5.contents = (
+  record5.contents =
     "<to>"
-        "<URI>alice@example.net</URI>"
-        "<name>Alice Adams</name>"
-      "</to>"
-      "<from>"
-        "<URI>bob@example.net</URI>"
-        "<name>Bob Barker</name>"
-      "</from>"
-      "<answered>0</answered>"
-      "<outgoing>1</outgoing>"
-    "<start-time>2002-05-30T09:30:13</start-time>");
+      "<URI>alice@example.net</URI>"
+      "<name>Alice Adams</name>"
+    "</to>"
+    "<from>"
+      "<URI>bob@example.net</URI>"
+      "<name>Bob Barker</name>"
+    "</from>"
+    "<answered>0</answered>"
+    "<outgoing>1</outgoing>"
+    "<start-time>2002-05-30T09:30:13</start-time>";
+
   record6.type = CallListStore::CallFragment::Type::REJECTED;
   record6.timestamp = "20020530093014";
   record6.id = "e";
-  record6.contents = (
+  record6.contents =
     "<to>"
-        "<URI>alice@example.net</URI>"
-        "<name>Alice Adams</name>"
-      "</to>"
-      "<from>"
-        "<URI>bob@example.net</URI>"
-        "<name>Bob Barker</name>"
-      "</from>"
-      "<answered>0</answered>"
-      "<outgoing>1</outgoing>"
-    "<start-time>2002-05-30T09:30:14</start-time>");
+      "<URI>alice@example.net</URI>"
+      "<name>Alice Adams</name>"
+    "</to>"
+    "<from>"
+      "<URI>bob@example.net</URI>"
+      "<name>Bob Barker</name>"
+    "</from>"
+    "<answered>0</answered>"
+    "<outgoing>1</outgoing>"
+    "<start-time>2002-05-30T09:30:14</start-time>";
+
   record7.type = CallListStore::CallFragment::Type::REJECTED;
   record7.timestamp = "20020530093015";
   record7.id = "f";
-  record7.contents = (
+  record7.contents =
     "<to>"
-        "<URI>alice@example.net</URI>"
-        "<name>Alice Adams</name>"
-      "</to>"
-      "<from>"
-        "<URI>bob@example.net</URI>"
-        "<name>Bob Barker</name>"
-      "</from>"
-      "<answered>0</answered>"
-      "<outgoing>1</outgoing>"
-    "<start-time>2002-05-30T09:30:15</start-time>");
+      "<URI>alice@example.net</URI>"
+      "<name>Alice Adams</name>"
+    "</to>"
+    "<from>"
+      "<URI>bob@example.net</URI>"
+      "<name>Bob Barker</name>"
+    "</from>"
+    "<answered>0</answered>"
+    "<outgoing>1</outgoing>"
+    "<start-time>2002-05-30T09:30:15</start-time>";
 
   records.push_back(record1);
-  records.push_back(record3);
   records.push_back(record2);
+  records.push_back(record3);
   records.push_back(record4);
   records.push_back(record5);
   records.push_back(record6);
@@ -216,23 +233,24 @@ void create_records(std::vector<CallListStore::CallFragment>& records)
 TEST_F(CallListStoreProcessorTest, CallListIsCountNeededNoLimit)
 {
   std::string timestamp = "";
-  bool rc =_clsp->_thread_pool->is_call_trim_needed("impu", timestamp, 0);
+  bool rc =_clsp->_thread_pool->is_call_trim_needed(IMPU, timestamp, FAKE_SAS_TRAIL);
   ASSERT_FALSE(rc);
 }
 
 TEST_F(CallListStoreProcessorTest, CallListWrite)
 {
   EXPECT_CALL(_load_monitor, request_complete(_)).Times(1);
-  EXPECT_CALL(*_cls, write_call_fragment_sync(_, _, _, _, _)).WillOnce(Return(CassandraStore::ResultCode::OK));
-  _clsp->write_call_list_entry("impu", "timestamp", "id", CallListStore::CallFragment::Type::BEGIN, "xml", 0);
+
+  EXPECT_CALL(*_cls, write_call_fragment_sync(IMPU, _, _, CALL_LIST_TTL, FAKE_SAS_TRAIL)).WillOnce(Return(CassandraStore::ResultCode::OK));
+  _clsp->write_call_list_entry(IMPU, TIMESTAMP, "id", CallListStore::CallFragment::Type::BEGIN, "xml", FAKE_SAS_TRAIL);
   sleep(1);
 }
 
 TEST_F(CallListStoreProcessorTest, CallListWriteWithError)
 {
   EXPECT_CALL(_load_monitor, request_complete(_)).Times(1);
-  EXPECT_CALL(*_cls, write_call_fragment_sync(_, _, _, _, _)).WillOnce(Return(CassandraStore::ResultCode::CONNECTION_ERROR));
-  _clsp->write_call_list_entry("impu", "timestamp", "id", CallListStore::CallFragment::Type::BEGIN, "xml", 0);
+  EXPECT_CALL(*_cls, write_call_fragment_sync(_, _, _, CALL_LIST_TTL, FAKE_SAS_TRAIL)).WillOnce(Return(CassandraStore::ResultCode::CONNECTION_ERROR));
+  _clsp->write_call_list_entry(IMPU, TIMESTAMP, "id", CallListStore::CallFragment::Type::BEGIN, "xml", FAKE_SAS_TRAIL);
   sleep(1);
 }
 
@@ -244,9 +262,9 @@ TEST_F(CallListStoreProcessorWithLimitTest, CallListWriteWithTrim)
   EXPECT_CALL(_load_monitor, request_complete(_)).Times(1);
   EXPECT_CALL(*_cls, write_call_fragment_sync(_, _, _, _, _)).WillOnce(Return(CassandraStore::ResultCode::OK));
   EXPECT_CALL(*_cls, get_call_fragments_sync(_,_,_)).WillOnce(DoAll(SetArgReferee<1>(records),
-                                                     Return(CassandraStore::ResultCode::OK)));
+                                                                    Return(CassandraStore::ResultCode::OK)));
   EXPECT_CALL(*_cls, delete_old_call_fragments_sync(_,_,_,_)).WillOnce(Return(CassandraStore::ResultCode::OK));
-  _clsp->write_call_list_entry("impu", "timestamp", "id", CallListStore::CallFragment::Type::BEGIN, "xml", 0);
+  _clsp->write_call_list_entry(IMPU, TIMESTAMP, "id", CallListStore::CallFragment::Type::BEGIN, "xml", FAKE_SAS_TRAIL);
   sleep(1);
 }
 
@@ -259,10 +277,10 @@ TEST_F(CallListStoreProcessorWithLimitTest, CallListIsCallTrimNeeded)
   create_records(records);
 
   EXPECT_CALL(*_cls, get_call_fragments_sync(_,_,_)).WillOnce(DoAll(SetArgReferee<1>(records),
-                                                     Return(CassandraStore::ResultCode::OK)));
+                                                                    Return(CassandraStore::ResultCode::OK)));
 
   std::string timestamp = "";
-  bool rc =_clsp->_thread_pool->is_call_trim_needed("impu", timestamp, 0);
+  bool rc =_clsp->_thread_pool->is_call_trim_needed(IMPU, timestamp, FAKE_SAS_TRAIL);
 
   ASSERT_TRUE(rc);
   ASSERT_EQ(timestamp, "20020530093011");
@@ -275,10 +293,10 @@ TEST_F(CallListStoreProcessorWithLimitTest, CallListIsCallTrimNeededCassError)
   std::vector<CallListStore::CallFragment> records;
 
   EXPECT_CALL(*_cls, get_call_fragments_sync(_,_,_)).WillOnce(DoAll(SetArgReferee<1>(records),
-                                          Return(CassandraStore::ResultCode::UNKNOWN_ERROR)));
+                                                                    Return(CassandraStore::ResultCode::UNKNOWN_ERROR)));
 
   std::string timestamp = "";
-  bool rc =_clsp->_thread_pool->is_call_trim_needed("impu", timestamp, 0);
+  bool rc =_clsp->_thread_pool->is_call_trim_needed(IMPU, timestamp, FAKE_SAS_TRAIL);
 
   ASSERT_FALSE(rc);
   ASSERT_EQ(timestamp, "");
@@ -289,7 +307,7 @@ TEST_F(CallListStoreProcessorWithLimitTest, CallListIsCallTrimNeededCassError)
 TEST_F(CallListStoreProcessorWithLimitTest, CallListPerformCallTrim)
 {
   EXPECT_CALL(*_cls, delete_old_call_fragments_sync(_,_,_,_)).WillOnce(Return(CassandraStore::ResultCode::OK));
-  _clsp->_thread_pool->perform_call_trim("impu", "timestamp", 123, 0);
+  _clsp->_thread_pool->perform_call_trim(IMPU, TIMESTAMP, 123, FAKE_SAS_TRAIL);
 }
 
 // Test where deleting the call records from the call list store fails when
@@ -297,5 +315,5 @@ TEST_F(CallListStoreProcessorWithLimitTest, CallListPerformCallTrim)
 TEST_F(CallListStoreProcessorWithLimitTest, CallListPerformCallTrimCassError)
 {
   EXPECT_CALL(*_cls, delete_old_call_fragments_sync(_,_,_,_)).WillOnce(Return(CassandraStore::ResultCode::UNKNOWN_ERROR));
-  _clsp->_thread_pool->perform_call_trim("impu", "timestamp", 0, 0);
+  _clsp->_thread_pool->perform_call_trim(IMPU, TIMESTAMP, 0, FAKE_SAS_TRAIL);
 }
--- src/ut/mementoappserver_test.cpp
+++ src/ut/mementoappserver_test.cpp
@@ -41,8 +41,10 @@
 #include "mockappserver.hpp"
 #include "mementoappserver.h"
 #include "mockloadmonitor.hpp"
+#include "mock_call_list_store.h"
 #include "mock_call_list_store_processor.h"
 #include "test_interposer.hpp"
+#include "mock_cassandra_store.h"
 
 using namespace std;
 using testing::InSequence;
@@ -60,6 +62,8 @@ public:
     _helper = new MockAppServerTsxHelper();
     _clsp = new MockCallListStoreProcessor();
 
+    // Completely control time so we can match against
+    // the start/answer/end times in the XML.
     cwtest_completely_control_time();
   }
 
@@ -84,8 +88,44 @@ public:
   static MockCallListStoreProcessor* _clsp;
 };
 
+/// Fixture for MementoAppServerTest.
+class MementoAppServerWithDialogIDTest : public SipCommonTest
+{
+public:
+  static void SetUpTestCase()
+  {
+    SipCommonTest::SetUpTestCase();
+    _helper = new MockAppServerTsxHelper("123_123_c2lwOjY1MDU1NTEyMzRAaG9tZWRvbWFpbg==");
+    _clsp = new MockCallListStoreProcessor();
+
+    cwtest_completely_control_time();
+  }
+
+  static void TearDownTestCase()
+  {
+    cwtest_reset_time();
+
+    delete _clsp; _clsp = NULL;
+    delete _helper; _helper = NULL;
+    SipCommonTest::TearDownTestCase();
+  }
+
+  MementoAppServerWithDialogIDTest() : SipCommonTest()
+  {
+  }
+
+  ~MementoAppServerWithDialogIDTest()
+  {
+  }
+
+  static MockAppServerTsxHelper* _helper;
+  static MockCallListStoreProcessor* _clsp;
+};
+
 MockAppServerTsxHelper* MementoAppServerTest::_helper = NULL;
 MockCallListStoreProcessor* MementoAppServerTest::_clsp = NULL;
+MockAppServerTsxHelper* MementoAppServerWithDialogIDTest::_helper = NULL;
+MockCallListStoreProcessor* MementoAppServerWithDialogIDTest::_clsp = NULL;
 
 namespace MementoAS
 {
@@ -198,12 +238,15 @@ string MementoAS::Message::get_response()
 
 using MementoAS::Message;
 
+std::string get_formatted_timestamp();
+
 // Test creation and destruction of the MementoAppServer objects
 TEST_F(MementoAppServerTest, CreateMementoAppServer)
 {
   // Create a MementoAppServer object
   std::string home_domain = "home.domain";
   MementoAppServer* mas = new MementoAppServer("memento",
+                                               NULL, // Call list store
                                                home_domain,
                                                0,
                                                25,
@@ -217,15 +260,23 @@ TEST_F(MementoAppServerTest, CreateMementoAppServer)
   MementoAppServerTsx* mast = (MementoAppServerTsx*)mas->get_app_tsx(_helper, req);
   EXPECT_TRUE(mast == NULL);
 
-  // Try with a valid method (Invite or Bye). This creates the application server
+  // Try with a valid method (Invite). This creates the application server
   // transaction
   msg._method = "INVITE";
   req = parse_msg(msg.get_request());
   mast = (MementoAppServerTsx*)mas->get_app_tsx(_helper, req);
   EXPECT_TRUE(mast != NULL);
+  delete mast; mast = NULL;
 
-  delete mast;
-  delete mas;
+  // Try with a valid method (Bye). This creates the application server
+  // transaction
+  msg._method = "BYE";
+  req = parse_msg(msg.get_request());
+  mast = (MementoAppServerTsx*)mas->get_app_tsx(_helper, req);
+  EXPECT_TRUE(mast != NULL);
+  delete mast; mast = NULL;
+
+  delete mas; mas = NULL;
 }
 
 // Test the mainline case for an incoming call
@@ -236,35 +287,26 @@ TEST_F(MementoAppServerTest, MainlineIncomingTest)
   std::string home_domain = "home.domain";
   MementoAppServerTsx as_tsx(_helper, _clsp, service_name, home_domain);
 
-  // Message is parsed successfully. The on_initial_request method
-  // adds a Record-Route header.
+  // Message is parsed successfully.
   EXPECT_CALL(*_helper, add_to_dialog(_));
   EXPECT_CALL(*_helper, send_request(_)).WillOnce(Return(0));
   as_tsx.on_initial_request(parse_msg(msg.get_request()));
 
   // On a 200 OK response the as_tsx generates a BEGIN call fragment
   // writes it to the call list store
-  time_t currenttime;
-  time(&currenttime);
-  tm* ct = localtime(&currenttime);
-  std::string answer_timestamp = as_tsx.create_formatted_timestamp(ct, "%Y-%m-%dT%H:%M:%S");
+  std::string timestamp = get_formatted_timestamp();
 
-  std::string xml = std::string("<to>\n\t<uri>sip:6505551234@homedomain</uri>\n</to>\n<from>\n\t<uri>sip:6505551000@homedomain</uri>\n\t<name>Alice</name>\n</from>\n<outgoing>1</outgoing>\n<start-time>").append(answer_timestamp).append("</start-time>\n<answered>1</answered>\n<answer-time>").append(answer_timestamp).append("</answer-time>\n\n");
+  // Check that the xml, impu and CallFragment type are as expected
+  std::string xml = std::string("<to>\n\t<uri>sip:6505551234@homedomain</uri>\n</to>\n<from>" \
+                                "\n\t<uri>sip:6505551000@homedomain</uri>\n\t<name>Alice</name>" \
+                                "\n</from>\n<outgoing>1</outgoing>\n<start-time>").
+                    append(timestamp).append("</start-time>\n<answered>1</answered>\n<answer-time>").
+                    append(timestamp).append("</answer-time>\n\n");
   std::string impu = "sip:6505551234@homedomain";
   EXPECT_CALL(*_clsp, write_call_list_entry(impu, _, _, CallListStore::CallFragment::Type::BEGIN, xml, _));
   EXPECT_CALL(*_helper, send_response(_));
   pjsip_msg* rsp = parse_msg(msg.get_response());
   as_tsx.on_response(rsp, 0);
-
-  // On a BYE in dialog request the as_tsx generates an END call
-  // fragment and writes it to the call list store.
-//  TODO temporaily commented out while the dialog id is being fixed.
-//  msg._route = "Route: <sip:123-456_789@memento.homedomain>";
-//  xml = std::string("<end-time>").append(answer_timestamp).append("</end-time>\n\n");
-//  EXPECT_CALL(*_helper, send_request(_)).WillOnce(Return(0));
-//  EXPECT_CALL(*_clsp, write_call_list_entry(_, _, _, _, _, _));
-//  EXPECT_CALL(*_clsp, write_call_list_entry(_, _, _, CallListStore::CallFragment::Type::END, xml, _));
-//  as_tsx.on_in_dialog_request(parse_msg(msg.get_request()));
 }
 
 // Test the mainline case for an outgoing call
@@ -275,30 +317,56 @@ TEST_F(MementoAppServerTest, MainlineOutgoingTest)
   std::string home_domain = "home.domain";
   MementoAppServerTsx as_tsx(_helper, _clsp, service_name, home_domain);
 
-  // Message is parsed successfully. The on_initial_request method
-  // adds a Record-Route header.
+  // Message is parsed successfully.
   msg._route = "Route: <sip:homedomain;orig>";
-  msg._extra = "P-Asserted-Identity: <sip:6505551234@homedomain;orig>";
+  pjsip_msg* req = parse_msg(msg.get_request());
+
+  // Add a P-Asserted _Identity header.
+  void* mem = pj_pool_alloc(_pool, sizeof(pjsip_routing_hdr));
+  pjsip_routing_hdr* p_asserted_id = (pjsip_routing_hdr*)mem;
+  pj_list_init(p_asserted_id);
+  p_asserted_id->type = PJSIP_H_OTHER;
+  p_asserted_id->name = pj_str("P-Asserted-Identity");
+  p_asserted_id->sname = pj_str("");
+  pjsip_name_addr_init(&p_asserted_id->name_addr);
+  pj_list_init(&p_asserted_id->other_param);
+
+  pjsip_name_addr* temp = (pjsip_name_addr*)uri_from_string("Alice <sip:6505550000@homedomain>", _pool, true);
+  memcpy(&p_asserted_id->name_addr, temp, sizeof(pjsip_name_addr));
+  pjsip_msg_add_hdr(req, (pjsip_hdr*)p_asserted_id);
+
   EXPECT_CALL(*_helper, add_to_dialog(_));
   EXPECT_CALL(*_helper, send_request(_)).WillOnce(Return(0));
-  as_tsx.on_initial_request(parse_msg(msg.get_request()));
+  as_tsx.on_initial_request(req);
 
   // On a 200 OK response the as_tsx generates a BEGIN call fragment
   // writes it to the call list store
-  time_t currenttime;
-  time(&currenttime);
-  tm* ct = localtime(&currenttime);
-  std::string answer_timestamp = as_tsx.create_formatted_timestamp(ct, "%Y-%m-%dT%H:%M:%S");
+  std::string timestamp = get_formatted_timestamp();
 
-  std::string xml = std::string("<to>\n\t<uri>sip:6505551234@homedomain</uri>\n</to>\n<from>\n\t<uri>sip:6505551000@homedomain</uri>\n\t<name>Alice</name>\n</from>\n<outgoing>0</outgoing>\n<start-time>").append(answer_timestamp).append("</start-time>\n<answered>1</answered>\n<answer-time>").append(answer_timestamp).append("</answer-time>\n\n");
-  std::string impu = "sip:6505551234@homedomain";
+  std::string xml = std::string("<to>\n\t<uri>sip:6505551234@homedomain</uri>\n\t<name>Bob</name>\n" \
+                                "</to>\n<from>\n\t<uri>sip:6505550000@homedomain</uri>\n\t<name>Alice</name>\n" \
+                                "</from>\n<outgoing>0</outgoing>\n<start-time>").
+                    append(timestamp).append("</start-time>\n<answered>1</answered>\n<answer-time>").
+                    append(timestamp).append("</answer-time>\n\n");
+  std::string impu = "sip:6505550000@homedomain";
   EXPECT_CALL(*_clsp, write_call_list_entry(impu, _, _, CallListStore::CallFragment::Type::BEGIN, xml, _));
   EXPECT_CALL(*_helper, send_response(_));
   pjsip_msg* rsp = parse_msg(msg.get_response());
   as_tsx.on_response(rsp, 0);
+}
 
-  // Send in another response - this should return straightaway
-  as_tsx.on_response(rsp, 0);
+// Test the mainline case for an outgoing call
+TEST_F(MementoAppServerTest, OutgoingMissingPAssertedHeaderTest)
+{
+  Message msg;
+  std::string service_name = "memento";
+  std::string home_domain = "home.domain";
+  MementoAppServerTsx as_tsx(_helper, _clsp, service_name, home_domain);
+
+  // Message is parsed and rejected.
+  msg._route = "Route: <sip:homedomain;orig>";
+  EXPECT_CALL(*_helper, send_request(_)).WillOnce(Return(0));
+  as_tsx.on_initial_request(parse_msg(msg.get_request()));
 }
 
 // Test that a non final response doesn't trigger writes to cassandra
@@ -319,6 +387,46 @@ TEST_F(MementoAppServerTest, OnNonFinalResponse)
   as_tsx.on_response(rsp, 0);
 }
 
+// Test that multiple responses don't provoke multiple writes to cassandra
+TEST_F(MementoAppServerTest, OnMultipleResponses)
+{
+  Message msg;
+  std::string service_name = "memento";
+  std::string home_domain = "home.domain";
+  MementoAppServerTsx as_tsx(_helper, _clsp, service_name, home_domain);
+
+  EXPECT_CALL(*_helper, add_to_dialog(_));
+  EXPECT_CALL(*_helper, send_request(_)).WillOnce(Return(0));
+  as_tsx.on_initial_request(parse_msg(msg.get_request()));
+
+  pjsip_msg* rsp = parse_msg(msg.get_response());
+  EXPECT_CALL(*_clsp, write_call_list_entry(_, _, _, _, _, _));
+  EXPECT_CALL(*_helper, send_response(_));
+  as_tsx.on_response(rsp, 0);
+
+  // Send in another response - this should return straightaway
+  EXPECT_CALL(*_helper, send_response(_));
+  as_tsx.on_response(rsp, 0);
+}
+
+// Test that a non final response doesn't trigger writes to cassandra
+TEST_F(MementoAppServerTest, OnNonInviteResponse)
+{
+  Message msg;
+  msg._method = "BYE";
+  std::string service_name = "memento";
+  std::string home_domain = "home.domain";
+  MementoAppServerTsx as_tsx(_helper, _clsp, service_name, home_domain);
+
+  EXPECT_CALL(*_helper, add_to_dialog(_));
+  EXPECT_CALL(*_helper, send_request(_)).WillOnce(Return(0));
+  as_tsx.on_initial_request(parse_msg(msg.get_request()));
+
+  pjsip_msg* rsp = parse_msg(msg.get_response());
+  EXPECT_CALL(*_helper, send_response(_));
+  as_tsx.on_response(rsp, 0);
+}
+
 // Test that an error response triggers a REJECTED call fragment.
 TEST_F(MementoAppServerTest, OnErrorResponse)
 {
@@ -332,8 +440,56 @@ TEST_F(MementoAppServerTest, OnErrorResponse)
   as_tsx.on_initial_request(parse_msg(msg.get_request()));
 
   msg._status = "404 Not Found";
-  EXPECT_CALL(*_clsp, write_call_list_entry(_, _, _, CallListStore::CallFragment::Type::REJECTED, _, _));
+  std::string timestamp = get_formatted_timestamp();
+
+  // Check that the xml, impu and CallFragment type are as expected
+  std::string xml = std::string("<to>\n\t<uri>sip:6505551234@homedomain</uri>\n</to>\n<from>" \
+                                "\n\t<uri>sip:6505551000@homedomain</uri>\n\t<name>Alice</name>" \
+                                "\n</from>\n<outgoing>1</outgoing>\n<start-time>").
+                    append(timestamp).append("</start-time>\n<answered>0</answered>\n\n");
+  std::string impu = "sip:6505551234@homedomain";
+  EXPECT_CALL(*_clsp, write_call_list_entry(impu, _, _, CallListStore::CallFragment::Type::REJECTED, xml, _));
   EXPECT_CALL(*_helper, send_response(_));
   pjsip_msg* rsp = parse_msg(msg.get_response());
   as_tsx.on_response(rsp, 0);
+}
+
+// Test an on in dialog response for an incoming call.
+TEST_F(MementoAppServerWithDialogIDTest, OnInDialogRequestTest)
+{
+  Message msg;
+  std::string service_name = "memento";
+  std::string home_domain = "home.domain";
+  MementoAppServerTsx as_tsx(_helper, _clsp, service_name, home_domain);
+
+  // Message is parsed successfully. The on_initial_request method
+  // adds a Record-Route header.
+  EXPECT_CALL(*_helper, add_to_dialog(_));
+  EXPECT_CALL(*_helper, send_request(_)).WillOnce(Return(0));
+  as_tsx.on_initial_request(parse_msg(msg.get_request()));
+
+  // On a 200 OK response the as_tsx generates a BEGIN call fragment
+  // writes it to the call list store
+  EXPECT_CALL(*_clsp, write_call_list_entry(_, _, _, _, _, _));
+  EXPECT_CALL(*_helper, send_response(_));
+  pjsip_msg* rsp = parse_msg(msg.get_response());
+  as_tsx.on_response(rsp, 0);
+
+  // On a BYE in dialog request the as_tsx generates an END call
+  // fragment and writes it to the call list store.
+  std::string impu = "sip:6505551234@homedomain";
+  std::string timestamp = get_formatted_timestamp();
+
+  std::string xml = std::string("<end-time>").append(timestamp).append("</end-time>\n\n");
+  EXPECT_CALL(*_helper, send_request(_)).WillOnce(Return(0));
+  EXPECT_CALL(*_clsp, write_call_list_entry(impu, _, _, CallListStore::CallFragment::Type::END, xml, _));
+  as_tsx.on_in_dialog_request(parse_msg(msg.get_request()));
+}
+
+std::string get_formatted_timestamp()
+{
+  time_t currenttime;
+  time(&currenttime);
+  tm* ct = localtime(&currenttime);
+  return create_formatted_timestamp(ct, "%Y-%m-%dT%H:%M:%S");
 }
