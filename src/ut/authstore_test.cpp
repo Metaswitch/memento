89bc3fb54e372393bd83a32d02d3ad60de3f85ad
Alex Hockey <alex.hockey@metaswitch.com>
2015-02-10 14:33:27 +0000

    Check that both formats can be read

--- src/ut/authstore_test.cpp
+++ src/ut/authstore_test.cpp
@@ -162,3 +162,95 @@ TYPED_TEST(BasicAuthStoreTest, ReadExpired)
   delete digest2; digest2 = NULL;
   delete digest3; digest3 = NULL;
 }
+
+
+/// Fixture for MultiFormatAuthStoreTest.  This uses a two AuthStores:
+/// 1). One that reads and writes only one format.
+/// 2). One that can read all formats.
+///
+/// The fixture is a template, parameterized over the different types of
+/// (de)serializer.
+template<class T>
+class MultiFormatAuthStoreTest : public ::testing::Test
+{
+  MultiFormatAuthStoreTest()
+  {
+    _local_data_store = new LocalStore();
+
+    {
+      AuthStore::SerializerDeserializer* serializer = new T();
+      std::vector<AuthStore::SerializerDeserializer*> deserializers = {
+        new T(),
+      };
+
+      _single_store = new AuthStore(_local_data_store,
+                                    serializer,
+                                    deserializers,
+                                    300);
+    }
+
+    {
+      AuthStore::SerializerDeserializer* serializer =
+        new AuthStore::JsonSerializerDeserializer();
+      std::vector<AuthStore::SerializerDeserializer*> deserializers = {
+        new AuthStore::JsonSerializerDeserializer(),
+        new AuthStore::BinarySerializerDeserializer(),
+      };
+
+      _multi_store = new AuthStore(_local_data_store,
+                                   serializer,
+                                   deserializers,
+                                   300);
+    }
+  }
+
+  virtual ~MultiFormatAuthStoreTest()
+  {
+    delete _local_data_store; _local_data_store = NULL;
+    delete _single_store; _single_store = NULL;
+    delete _multi_store; _multi_store = NULL;
+    cwtest_reset_time();
+  }
+
+  LocalStore* _local_data_store;
+  AuthStore* _single_store;
+  AuthStore* _multi_store;
+};
+
+// MultiFormatAuthStoreTest is parameterized over these types.
+TYPED_TEST_CASE(MultiFormatAuthStoreTest, SerializerDeserializerTypes);
+
+
+TYPED_TEST(MultiFormatAuthStoreTest, CanReadAllFormats)
+{
+  // Write a digest to the store.
+  std::string impi = "6505551234@cw-ngv.com";
+  std::string nonce = "9876543210";
+
+  AuthStore::Digest* digest = new AuthStore::Digest();
+  digest->_impi = "6505551234@cw-ngv.com";
+  digest->_nonce = "9876543210";
+  digest->_ha1 = "123123123";
+  digest->_opaque = "opaque";
+  digest->_realm = "cw-ngv.com";
+  digest->_nonce_count = 3;
+  digest->_impu = "sip:" + impi;
+
+  this->_single_store->set_digest(impi, nonce, digest, 0);
+
+  // Retrieve the digest from the store and check it against
+  // the initial digest
+  AuthStore::Digest* digest2;
+  this->_multi_store->get_digest(impi, nonce, digest2, 0);
+
+  ASSERT_EQ(digest->_impi, digest2->_impi);
+  ASSERT_EQ(digest->_nonce, digest2->_nonce);
+  ASSERT_EQ(digest->_ha1, digest2->_ha1);
+  ASSERT_EQ(digest->_realm, digest2->_realm);
+  ASSERT_EQ(digest->_opaque, digest2->_opaque);
+  ASSERT_EQ(digest->_impu, digest2->_impu);
+  ASSERT_EQ(digest->_nonce_count, digest2->_nonce_count);
+
+  delete digest; digest = NULL;
+  delete digest2; digest2 = NULL;
+}
