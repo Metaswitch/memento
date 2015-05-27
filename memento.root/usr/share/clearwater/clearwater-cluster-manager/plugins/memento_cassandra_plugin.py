d738330b02179e92b73172249919b825391457b4
Alex Hockey <alex.hockey@metaswitch.com>
2015-05-27 16:09:25 +0100

    Refactor plugins to take a params object

--- memento.root/usr/share/clearwater/clearwater-cluster-manager/plugins/memento_cassandra_plugin.py
+++ memento.root/usr/share/clearwater/clearwater-cluster-manager/plugins/memento_cassandra_plugin.py
@@ -44,9 +44,9 @@ _log = logging.getLogger("memento_cassandra_plugin")
 
 
 class MementoCassandraPlugin(SynchroniserPluginBase):
-    def __init__(self, ip, local_site, remote_site):
-        self._ip = ip
-        self._local_site = local_site
+    def __init__(self, params):
+        self._ip = params.ip
+        self._local_site = params.local_site
         _log.debug("Raising Cassandra not-clustered alarm")
         issue_alarm(constants.RAISE_CASSANDRA_NOT_YET_CLUSTERED)
 
@@ -63,7 +63,7 @@ class MementoCassandraPlugin(SynchroniserPluginBase):
                                self._ip,
                                self._local_site)
 
-        if (self._ip == sorted(cluster_view.keys())[0]): 
+        if (self._ip == sorted(cluster_view.keys())[0]):
             _log.debug("Adding Memento schema")
             run_command("/usr/share/clearwater/cassandra-schemas/memento.sh")
 
@@ -86,5 +86,5 @@ class MementoCassandraPlugin(SynchroniserPluginBase):
         return ["/etc/cassandra/cassandra.yaml"]
 
 
-def load_as_plugin(ip, local_site, remote_site):
-    return MementoCassandraPlugin(ip, local_site, remote_site)
+def load_as_plugin(params):
+    return MementoCassandraPlugin(params)
