087b29c032a647af709b2ec4e7451aa4df5dfdfd
Graeme Robertson <graeme.robertson@metaswitch.com>
2015-12-15 17:59:29 +0000

    Add GR scehma

--- memento.root/usr/share/clearwater/cassandra-schemas/memento.sh
+++ memento.root/usr/share/clearwater/cassandra-schemas/memento.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+. /etc/clearwater/config
+
 if [[ ! -e /var/lib/cassandra/data/memento ]];
 then
   count=0
@@ -15,7 +17,24 @@ then
     /usr/share/clearwater/bin/poll_cassandra.sh --no-grace-period
   done
 
-  echo "CREATE KEYSPACE memento WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 2};
+  replication_str="{'class': 'SimpleStrategy', 'replication_factor': 2}"
+
+  # If local_site_name and remote_site_names are set then this is a GR
+  # deployment. Set the replication strategy to NetworkTopologyStrategy and
+  # define the sites.
+  if [ -n $local_site_name ] && [ -n $remote_site_names ]
+  then
+    IFS=',' read -a remote_site_names_array <<< "$remote_site_names"
+    replication_str="{'class': 'NetworkTopologyStrategy', '$local_site_name': 2"
+    for remote_site in "${remote_site_names_array[@]}"
+    do
+      # Set the replication factor for each site to 2.
+      replication_str+=", '$remote_site': 2"
+    done
+    replication_str+="}"
+  fi
+
+  echo "CREATE KEYSPACE memento WITH REPLICATION = $replication_str;
         USE memento;
         CREATE TABLE call_lists (impu text PRIMARY KEY, dummy text) WITH COMPACT STORAGE and read_repair_chance = 1.0;
   " | cqlsh
